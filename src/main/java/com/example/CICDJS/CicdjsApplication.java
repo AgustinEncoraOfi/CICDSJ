5959fb195f9bc382392b0de932cb389e2f9ce757
AgustinEncoraOfi <agustin.encora.ofi@gmail.com>
2024-03-21 12:08:23 -0400

    test

---
+++ src/main/java/com/example/CICDJS/CicdjsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CICDJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CICDJS/CicdjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CICDJS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
